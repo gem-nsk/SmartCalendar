9af489fb8f6d40765db7de886f3c1e06c8952c65
Bjorn Sodenberg <garonini1@gmail.com>
2019-10-17 17:45:52 +0700

    продолжается работа с верхним календарем

--- app/src/main/java/com/example/smartcalendar/DateAdapter.kt
+++ app/src/main/java/com/example/smartcalendar/DateAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.smartcalendar
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.date_layout.view.*
+import java.util.*
 
-class DateAdapter:
+class DateAdapter(val dateList: List<Calendar>):
         RecyclerView.Adapter<MyViewHolder>(){
 
     val week = mutableListOf("Пн", "Вт", "Ср", "Чт","Пт","Сб","Вс","Пн", "Вт", "Ср", "Чт","Пт","Сб","Вс")
@@ -19,18 +22,42 @@ class DateAdapter:
     }
 
     override fun getItemCount(): Int {
-        return week.size
+        return dateList.count()
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val getWeekDay = week.get(position)
-        holder?.view?.str_day_element?.text = getWeekDay
-        holder?.view?.num_day_element?.text = position.toString()
+//        val getWeekDay = week.get(position)
+//        holder?.view?.str_day_element?.text = getWeekDay
+//        holder?.view?.num_day_element?.text = position.toString()
+        var flg = false
+        for(date in dateList){
+            val dayOfWeekInMonth: Int = date.get(Calendar.DAY_OF_WEEK_IN_MONTH)
+            var dayOfWeekInMonthStr: String = ""
+            when(dayOfWeekInMonth) {
+                0 -> dayOfWeekInMonthStr = "Пн"
+                1 -> dayOfWeekInMonthStr = "Вт"
+                2 -> dayOfWeekInMonthStr = "Ср"
+                3 -> dayOfWeekInMonthStr = "Чт"
+                4 -> dayOfWeekInMonthStr = "Пт"
+                5 -> dayOfWeekInMonthStr = "Сб"
+                6 -> dayOfWeekInMonthStr = "Вс"
+            }
+            holder?.view?.str_day_element?.text = dayOfWeekInMonthStr
+            holder?.view?.num_day_element?.text = date.get(Calendar.DATE).toString()
+            if(flg == false){
+                holder?.view?.str_day_element?.setTextColor(Color.BLUE)
+                holder?.view?.num_day_element?.setTextColor(Color.BLUE)
+                flg = true
+            }
+            else{
+                holder?.view?.str_day_element?.setTextColor(Color.BLUE)
+                holder?.view?.num_day_element?.setTextColor(Color.BLUE)
+            }
+        }
+
+
 
     }
-
-
-
 }
 
 class MyViewHolder(val view: View): RecyclerView.ViewHolder(view){
--- app/src/main/java/com/example/smartcalendar/MainActivity.kt
+++ app/src/main/java/com/example/smartcalendar/MainActivity.kt
@@ -8,15 +8,40 @@ import androidx.recyclerview.widget.RecyclerView
 import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.Direction
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.Year
+import java.util.*
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity(){
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var date = Calendar.getInstance()
         singeRowCalendar.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        singeRowCalendar.adapter = DateAdapter()
+        singeRowCalendar.adapter = DateAdapter(GetDaysList())
 
     }
+
+    fun GetDaysList(): List<Calendar> {
+        val daysList = mutableListOf<Calendar>()
+        val date = Calendar.getInstance()
+        var currentDate = date
+        for( i in 1..30){
+            daysList.add(currentDate)
+            currentDate.add(Calendar.DAY_OF_MONTH, 1)
+        }
+        return daysList
+    }
+
 }
