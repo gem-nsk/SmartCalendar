d8cd1e05d925f553dc5c74963784b5fd2f8ea3c8
Bjorn Sodenberg <garonini1@gmail.com>
2019-10-17 09:41:07 +0700

    first commit

---
+++ app/src/main/java/com/example/smartcalendar/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.smartcalendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.Direction
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
